eb6cbbe5b4703acec82c127db06425d9b8ea7e25
tripathigaurav77 <tripathigaurav77@gmail.com>
2019-08-24 16:21:37 +0530

    2nd commit

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -9,12 +9,15 @@ namespace Vidly.Controllers
 {
 	public class MoviesController : Controller
 	{
-		//
 		// GET: /Movies/Random
 		public ActionResult Random()
 		{
             var movie = new Movie() { Name = "Shrek" };
 			return View(movie);
+            //return Content("Hello World!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
 		}
 	}
 }
